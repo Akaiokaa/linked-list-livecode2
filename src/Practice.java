0b4f64d497718d5fb93db082ee6f543a95243ed4
Akaioka <brendanvillaraza@yahoo.com>
2026-01-13 14:48:24 -0800

    Completed live code

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,28 @@
+import java.text.NumberFormat.Style;
+
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        // System.out.println(head.value);
+        // System.out.println(head.next);
+        Node head = new Node('z', null);
+        Node ryan = new Node('s', null);
+
+        head.next = ryan;
+
+        ryan.next = new Node('t', null);
+
+        ryan.next.next = new Node('x', null);
+        head.next.next.next.next = new Node('d', null);
+        // System.out.println(head.next.next.value);
+
+        printList(head);
+    }
+    public static void printList(Node start){
+        Node current = start;
+
+        while(current != null){
+            System.out.println(current.value);;
+            current = current.next;
+        }
     }
 }

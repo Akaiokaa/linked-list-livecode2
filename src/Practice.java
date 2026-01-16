1134b457bf79cd2014d66bae28885f3be17bfeb2
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-01-15 18:24:07 -0800

    Completed live code follow along

--- src/Practice.java
+++ src/Practice.java
@@ -6,14 +6,17 @@ public class Practice {
         // System.out.println(head.next);
         Node head = new Node('z', null);
         Node ryan = new Node('s', null);
-
         head.next = ryan;
 
         ryan.next = new Node('t', null);
 
         ryan.next.next = new Node('x', null);
-        head.next.next.next.next = new Node('d', null);
+        //true
+        System.out.println(contains(head, 's'));
+        //false
+        System.out.println(contains(head, 'a'));
         // System.out.println(head.next.next.value);
+        remove(head, 't');
 
         printList(head);
     }
@@ -24,5 +27,27 @@ public class Practice {
             System.out.println(current.value);;
             current = current.next;
         }
+    }
+
+    public static boolean contains( Node start, char toFind){
+        Node current = start; 
+        while ( current != null ) {
+            if(current.value == toFind){ return true; }
+            current = current.next;
+        };
+        return false;
+    };
+
+    // Remove the first node that has toRemove
+    public static char remove(Node head, char toRemove){
+        Node current = head;
+        while (current.next  != null) {
+            if (current.next.value == toRemove) {
+                current.next = current.next.next;
+                return toRemove;
+            }
+            current = current.next;
+        }
+        return '\0';
     }
 }
